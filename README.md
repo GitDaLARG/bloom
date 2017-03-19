904c77134281bf54ae6f173289c32acb1f446fcf
Lucas Meine <Lucas Meine>
2017-03-19 03:29:28 -0300

    commits

---
+++ README.md
@@ -0,0 +1,4 @@
+# bloom - by Renan Mayrinck
+A framework to make the development of websites simple and fast for both, the developer and end-user.
+
+![alt tag](https://mir-s3-cdn-cf.behance.net/project_modules/1400/0444f534598231.57e3a3137317d.png)
---
+++ js/bloom.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+//code for parsing the chips.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    $('chip').each(function(index, object) {
+
+        var attr = $(object).attr('id');
+        var classe = $(object).attr('class');
+
+        if (typeof classe === typeof undefined || classe === false) {
+
+          $(object).attr('class','');
+          console.log($(object).attr('class'));
+        }
+
+        if (typeof attr !== typeof undefined && attr !== false) {
+
+            start = '<div id="'+attr+'" class="bloom-chip '+$(object).attr('class').replace(/\\bloom-chip/, "")+'">';
+          }
+        else {
+
+            start = '<div class="bloom-chip '+$(object).attr('class').replace(/\\bloom-chip/, "")+'">';
+        }
+
+        var attr = $(object).attr('persistent');
+
+        if (typeof attr !== typeof undefined && attr !== false) {
+
+          $(object).replaceWith(start+' <img class="pic" src="' + $(object).attr('img') + '"> <p>' + $(object).text() + '</p></div>')
+        } else {
+          $(object).replaceWith(start+' <img class="pic" src="' + $(object).attr('img') + '"> <p>' + $(object).text() + '</p> <button class="close">x</button> </div>')
+        }
+
+
+    })
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+});
